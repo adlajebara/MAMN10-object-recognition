dadcf13ba115ab16c8321c0ab7788a2861346d39
Stella <st4670jo-s@student.lu.se>
2022-11-16 14:59:55 +0100

    Split train and test set

--- splitdata.py
+++ splitdata.py
@@ -1,5 +1,5 @@
-import splitfolder
+import splitfolders
 # Split with a ratio.
 # To only split into training and validation set, set a tuple to `ratio`, i.e, `(.8, .2)`.
-splitfolders.ratio("datasets/face_dataset_train_images/1-14/", output="t-1-14",
+splitfolders.ratio("datasets/face_dataset_train_images/", output="output",
     seed=1337, ratio=(.8, .2), group_prefix=None, move=True) # default values
